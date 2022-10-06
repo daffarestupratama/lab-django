f95b68af98ee0afe24b04836eb0b258cf67bd2fe
daffarestupratama <daffa.ilham@ui.ac.id>
2022-10-06 16:42:11 +0700

    Tutorial Lab 5

--- wishlist/views.py
+++ wishlist/views.py
@@ -8,6 +8,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required(login_url='/wishlist/login/')
 def show_wishlist(request):
@@ -20,6 +21,29 @@ def show_wishlist(request):
         'username': username
     }
     return render(request, "wishlist.html", context)
+
+@login_required(login_url='/wishlist/login/')
+def show_wishlist_ajax(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    username = request.user.username
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Daffa Ilham Restupratama',
+        'last_login': request.COOKIES['last_login'],
+        'username': username
+    }
+    return render(request, "wishlist_ajax.html", context)
+
+@login_required(login_url='/wishlist/login/')
+@csrf_exempt
+def submit_wishlist_ajax(request):
+    if request.method == 'POST':
+        nama_barang = request.POST.get('nama_barang')
+        harga_barang = request.POST.get('harga_barang')
+        deskripsi = request.POST.get('deskripsi')
+        BarangWishlist.objects.create(nama_barang=nama_barang,deskripsi=deskripsi,harga_barang=harga_barang)
+        response = HttpResponseRedirect(reverse("wishlist:show_wishlist_ajax")) 
+        return response
 
 def show_xml(request):
     data = BarangWishlist.objects.all()
